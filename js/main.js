704dce772d5cf1e5db7c725a41da46b9f4481980
Gustavo Serafim <tioogu@gmail.com>
2022-06-01 02:27:50 -0300

    correção evento js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-function enviarDados(e){
-    e.preventDefault();
-    const nome= document.getElementById(`nome`).value
-    const email= document.getElementById(`email`).value
-    const data= document.getElementById(`data`).value
-    const senha= document.getElementById(`senha`).value
+function enviarDados(){
+    const nome = document.getElementById(`nome`).value
+    const email = document.getElementById(`email`).value
+    const data = document.getElementById(`data`).value
+    const senha = document.getElementById(`senha`).value
     console.log(nome, email, data, senha);
 }
 
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault();
+    enviarDados();
+  });
